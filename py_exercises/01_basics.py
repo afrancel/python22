6d91a4955b56f56c7b00f656403b1ebf7a5373ef
Francel <botonescreativosyf@gmail.com>
2022-07-13 23:05:00 -0400

    update

--- py_exercises/01_basics.py
+++ py_exercises/01_basics.py
@@ -1,4 +1,19 @@
+# 0001 Escribir un programa que muestre por pantalla la cadena ¡Hola Mundo!.
+print("Hola mundo")
 
+# 0002 asignar un valor a una variable e imprimir esa variable
+saludo = "Hola mundo"
+print (saludo)
 
-saludodos = input("Indique su nombre")
-print(saludodos + "hola")
+# 0003
+"""
+Escribir un programa que pregunte el nombre del usuario en la consola y
+# después de que el usuario lo introduzca muestre por pantalla la cadena ¡Hola <nombre>!,
+donde <nombre> es el nombre que el usuario haya introducido.
+"""
+pregunta = input("Su nombre por favor")
+print("hola " + pregunta)
+
+name1 = "luis"
+apellido = "Acevedo"
+print(f"{name1},{apellido}")
